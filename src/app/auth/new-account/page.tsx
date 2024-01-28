e74bcb46b7e55e5e9ce0bcc2741ac184363ff831
Bladimir G. Parra B. | IP Proyectos y Soluciones <bladyparra@gmail.com>
2024-01-27 23:56:36 -0500

    First commit - *Server Components *Client Components * Zustand *Tailwind *CLSX * Directory structure * Nested layouts * Reusable components * Blur effects with Tailwind * Effects with product images * Routes from our application

---
+++ src/app/auth/new-account/page.tsx
@@ -0,0 +1,10 @@
+import { titleFonts } from "@/config/fonts";
+import Image from "next/image";
+
+export default function NewAccount() {
+  return (
+    <main className="">
+      <h1>New Account Page</h1>
+    </main>
+  );
+}
